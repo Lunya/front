b0f34ecda5871a0526d3f7983a08e2feb7f75fd5
Xavier Blanc <xavier.blanc@labri.fr>
2018-01-14 16:33:15 +0100

    prod debug

--- letsencrypt_and_start_nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "deb http://ftp.debian.org/debian stretch-backports main" >> /etc/apt/sources.list
-cat /etc/apt/sources.list
-apt-get update 
-apt-get install -y python-certbot-nginx -t stretch-backports
-certbot --nginx certonly -n -m blancxav@gmail.com --agree-tos -d wat.promyze.com
-nginx -g "daemon off;"
